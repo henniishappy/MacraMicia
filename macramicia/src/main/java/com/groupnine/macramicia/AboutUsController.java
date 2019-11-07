96affb75f9a416912d20d26178ce8675c24849d1
runnninggag <s0564263@htw-berlin.de>
2019-11-07 22:59:03 +0100

    forgot 2 files in last commit

---
+++ macramicia/src/main/java/com/groupnine/macramicia/AboutUsController.java
@@ -0,0 +1,16 @@
+package com.groupnine.macramicia;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+
+@Controller
+public class AboutUsController {
+	@GetMapping("/AboutUs")
+	public String date(Model model) {
+		model.addAttribute("date", new Date(System.currentTimeMillis()));
+		return "AboutUs";
+	}
+}
