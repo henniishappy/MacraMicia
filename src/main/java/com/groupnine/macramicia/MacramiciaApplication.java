154e40c6f27691d1ba86a9df1b6d2e9352c3b571
runnninggag <lukas_ludwig@gmx.net>
2019-11-27 20:19:43 +0100

    remove macramicia subfolder

---
+++ src/main/java/com/groupnine/macramicia/MacramiciaApplication.java
@@ -0,0 +1,13 @@
+package com.groupnine.macramicia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacramiciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacramiciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupnine/macramicia/MacramiciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupnine.macramicia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacramiciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
