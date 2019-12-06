eef11e97528650214112189a8c09660a85576bb1
Henriette <hennisand@gmail.com>
2019-12-06 14:44:08 +0100

    fixed file structure

--- src/main/java/com/macramicia/WebAppInitialiser.java
+++ src/main/java/com/macramicia/WebAppInitialiser.java
@@ -1,4 +1,4 @@
-package com.groupnine.macramicia;
+package com.macramicia;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
