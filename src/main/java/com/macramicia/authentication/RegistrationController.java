c1f720258cc31b4ceea6e48b6805670eed7ee128
nparschew <s0563436@htw-berlin.de>
2020-01-08 11:58:14 +0100

    changed dependencies and added Repository interfaces

--- src/main/java/com/macramicia/authentication/RegistrationController.java
+++ src/main/java/com/macramicia/authentication/RegistrationController.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.WebRequest;
 
 @Controller
 @RequestMapping("/user")
 public class RegistrationController {
 
-    @GetMapping
-    public String showRegistrationForm(WebRequest request, Model model) {
+    @GetMapping("/registration")
+    public String showRegistrationForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
         return "registration";
     }
+
 }
