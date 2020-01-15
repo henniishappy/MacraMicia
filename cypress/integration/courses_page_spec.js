9e4727d3a840b431d42f266163e773381af07f34
Henriette <hennisand@gmail.com>
2020-01-15 19:12:18 +0100

    updated course display

--- cypress/integration/courses_page_spec.js
+++ cypress/integration/courses_page_spec.js
@@ -19,25 +19,25 @@ describe('The course overview page', function() {
     cy.contains('Courses')
   })
 
-  it('displays course details in a table', function() {
+  it('displays course details in cards', function() {
     cy
-        .get('[data-cy=tableCourses]')
+        .get('[data-cy=courseCard]')
         .should('be.visible')
-        .children().should('have.length', 2)
+        // .children().should('have.length', 2)
 
-        .get('[data-cy=tableCoursesHeaderTitle]')
-        .contains('Title')
-        .get('[data-cy=tableCoursesHeaderDescription]')
-        .contains('Description')
-        .get('[data-cy=tableCoursesHeaderDate]')
-        .contains('Date')
-        .get('[data-cy=tableCoursesHeaderVenue]')
-        .contains('Venue')
-        .get('[data-cy=tableCoursesHeaderSpots]')
-        .contains('Remaining Spots')
-
-        .get('[data-cy=tableCoursesBody]')
-        .children().should('have.length.greaterThan', 0)
+        // .get('[data-cy=tableCoursesHeaderTitle]')
+        // .contains('Title')
+        // .get('[data-cy=tableCoursesHeaderDescription]')
+        // .contains('Description')
+        // .get('[data-cy=tableCoursesHeaderDate]')
+        // .contains('Date')
+        // .get('[data-cy=tableCoursesHeaderVenue]')
+        // .contains('Venue')
+        // .get('[data-cy=tableCoursesHeaderSpots]')
+        // .contains('Remaining Spots')
+        //
+        // .get('[data-cy=tableCoursesBody]')
+        // .children().should('have.length.greaterThan', 0)
  })
 
 })
