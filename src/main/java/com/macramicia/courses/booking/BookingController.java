c14fe8c8b1859448a3e0bc0e917aa2724a329986
runnninggag <lukas_ludwig@gmx.net>
2020-01-30 19:45:23 +0100

    fixed contains problem

--- src/main/java/com/macramicia/courses/booking/BookingController.java
+++ src/main/java/com/macramicia/courses/booking/BookingController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/courses/booking")
@@ -29,18 +30,33 @@ public class BookingController {
 	public String updateMyCourses(@ModelAttribute("newCourse") Course newCourse, Principal principal) {
 		User currentUser = userService.findUserByUsername(principal.getName());
 
+		Set<Course> courses = currentUser.getCourses();
+		for (Course course : courses) {
+			if (course.getId() == newCourse.getId())
+				return "redirect:/";
+		}
+
+		if (newCourse.addParticipant(currentUser)) {
+			currentUser.addCourse(newCourse);
+			userService.save(currentUser);
+		}
+		return "redirect:/user/profile/show";
+
+		/*
 		if (!currentUser.getCourses().contains(newCourse)) {
 
 			//newCourse.addParticipant(currentUser);
 			userService.save(currentUser);
-			/*
+
+
 			if (newCourse.addParticipant(currentUser)) {
 				currentUser.addCourse(newCourse);
 
 				userService.save(currentUser);
-			}*/
+			}
 			return "redirect:/user/profile/show";
 		}
 		return "redirect:/";
+		*/
 	}
 }
